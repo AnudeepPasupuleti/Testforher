36b1fb549379c6d702f771b3be8bc87f26731751
Anudeep Pasupuleti <117937774+AnudeepPasupuleti@users.noreply.github.com>
2026-01-08 16:31:43 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'v2-cache'; // Changing this number forces an update
+
+self.addEventListener('install', (event) => {
+    // Force the waiting service worker to become the active one
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    // Take control of the page immediately
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Serve from network, but you can add caching later
+    event.respondWith(fetch(event.request));
+});
